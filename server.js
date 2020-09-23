cef65e17001fb249da6eb50e1075edb8bf8ff1bb
stoozy <mahinsemail@gmail.com>
2020-09-23 12:17:36 -0400

    Change port

--- server.js
+++ server.js
@@ -9,4 +9,5 @@ app.get('/', (req, res) =>{
     res.redirect("/public");
 });
 
-app.listen(3000, () => console.log(' ⚡️[server]: Server is running at https://localhost:3000'));
+var PORT = 8080;
+app.listen(PORT, () => console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`));
